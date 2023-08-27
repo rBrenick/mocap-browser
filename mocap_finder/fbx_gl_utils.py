69b523fc0ea9567a3199ea2144318ec77a60bef8
rBrenick <RichardBrenick@gmail.com>
2023-08-27 21:03:52 +0200

    optimize

--- mocap_finder/fbx_gl_utils.py
+++ mocap_finder/fbx_gl_utils.py
@@ -2,23 +2,15 @@ from .gl_utils.scene_utils import  draw_locator, draw_line
 import FbxCommon
 SKELETON_NODE_TYPE = FbxCommon.FbxNodeAttribute.eSkeleton
 
-def recursive_draw_fbx_skeleton(node, time, color):
-    for i in range(node.GetChildCount()):
-        recursive_draw_fbx_skeleton(node.GetChild(i), time, color)
-    
+def recursive_draw_fbx_skeleton(node, time, color, parent_pos=None):
+    node_pos = None
     node_attribute = node.GetNodeAttribute()
-    if not node_attribute:
-        return
+    if node_attribute:
+        if node_attribute.GetAttributeType() == SKELETON_NODE_TYPE:
+            node_pos = node.EvaluateGlobalTransform(time).GetT()
+            if parent_pos is not None:
+                draw_line(node_pos, parent_pos, color)
 
-    if node_attribute.GetAttributeType() != SKELETON_NODE_TYPE:
-        return
-    
-    node_transform = node.EvaluateGlobalTransform(time) # type: fbx.FbxAMatrix
-
-    parent_transform = node.GetParent().EvaluateGlobalTransform(time) # type: fbx.FbxAMatrix
-    parent_pos = parent_transform.GetT()
-    parent_at_origo = all(coord == 0.0 for coord in parent_pos)
-
-    if not parent_at_origo:
-        draw_line(node_transform.GetT(), parent_pos, color)
-    
+    for i in range(node.GetChildCount()):
+        recursive_draw_fbx_skeleton(node.GetChild(i), time, color, node_pos)
+        
